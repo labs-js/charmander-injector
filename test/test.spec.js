34016cd58cba298b5e11ca5e18917c59fed922e9
Alan Campora <alan.campora@sovanta.com>
2016-11-16 21:45:17 -0300

    [ADD] register function working

--- test/test.spec.js
+++ test/test.spec.js
@@ -1,9 +1,30 @@
-import charmander from '../lib/charmander-injector';
+import CharmanderInjector from '../lib/charmander-injector';
 
-describe('charmander-injector',() =>{
+describe('charmander-injector', () => {
 
-    it('should return true',()=>{
-    
-        expect(true).toBe(true); 
+    var charmander;
+
+    beforeEach(() => {
+        charmander = new CharmanderInjector();
     });
-}); 
+
+    describe('register', () => {
+        it('should be defined', ()=>{
+            expect(charmander.register).toBeDefined();
+        });
+
+
+        it('should register dependency', () => {
+            var dummyInquirer = {name:"dummy inquirer"},
+                dummyExec = {name:"dummy exec"};
+
+            charmander.register('inquirer', dummyInquirer);
+            charmander.register('exec', dummyExec);
+
+
+            expect(charmander.dependencies['inquirer']).toBeDefined();
+            expect(charmander.dependencies['exec']).toBeDefined();
+        });
+
+    });
+});
