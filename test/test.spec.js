5b7300db41dabee356a499aa76c04c9300d8e136
Alan Campora <alan.campora@sovanta.com>
2016-11-21 18:25:15 -0300

    [ADD] inject test

--- test/test.spec.js
+++ test/test.spec.js
@@ -9,22 +9,54 @@ describe('charmander-injector', () => {
     });
 
     describe('register', () => {
-        it('should be defined', ()=>{
+        it('should be defined', () => {
             expect(charmander.register).toBeDefined();
         });
 
 
         it('should register dependency', () => {
-            var dummyInquirer = {name:"dummy inquirer"},
-                dummyExec = {name:"dummy exec"};
+            var dummyInquirer = {
+                    name: "dummy inquirer"
+                },
+                dummyExec = {
+                    name: "dummy exec"
+                };
 
             charmander.register('inquirer', dummyInquirer);
             charmander.register('exec', dummyExec);
 
-
             expect(charmander.dependencies['inquirer']).toBeDefined();
             expect(charmander.dependencies['exec']).toBeDefined();
         });
-
     });
+
+    describe('inject', () => {
+
+        it('should be defined', () => {
+            expect(charmander.inject).toBeDefined();
+        });
+
+        it('should inject dependency into object', () => {
+
+            var dummyDependencies = {
+                'dependency': {
+                    value: 'this is a dummy obj'
+                }
+            }
+
+            spyOn(charmander, 'getDependency').andReturn(dummyDependencies.dependency);
+
+            console.log('getDependency', charmander.getDependency('saraza'));
+
+            var result = charmander.inject(
+                ['dependency'], (dependency) => {
+                
+                    console.log(dependency); 
+                },this);
+
+            console.log(result.arguments);
+
+        });
+    });
+
 });
